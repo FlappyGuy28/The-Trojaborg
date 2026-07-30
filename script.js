e51a8ec6ba5e5e8e8a876566b36769b75f18e376
FlappyGuy28 <brendanzawodny@gmail.com>
2026-07-29 21:29:19 -0400

    a

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const button = document.getElementById('ctaButton');
+  const button = document.getElementById('Button');
 
   if (button) {
     button.addEventListener('click', () => {
       button.textContent = 'Thanks!';
+      button.download = The_Start.zip;
     });
   }
 });
